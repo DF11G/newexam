16872306a4f13e9d439706ffcaeb6767a5420051
DF11G <2326444219@qq.com>
2020-11-23 09:55:32 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Table, PageHeader, Tag, Button } from 'antd'
+import 'antd/dist/antd.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Button type="primary">123</Button>
+            </div>
+        )
+    }
+}
+
+export default App
